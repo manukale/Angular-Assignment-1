0e4122d789a6575de097bea8dd63d14fafd1df9d
Manasi Kale <miss.manasikale@gmail.com>
2024-12-16 19:08:52 +0530

    new function change

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserDataService } from '../../services/user-data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  student: any[] = [];
 
+  constructor(private router: Router, private userService: UserDataService) { }
+
+  signOut() {
+    this.router.navigate(['/login'],{})
+  }
+
+
+  showStudent() {
+    this.userService.fetchUserData().subscribe((res) => {
+      console.log(res);
+      // this.student = res
+      for (let i = 0; i < res.length; i++) {
+        if (res[i].role === 'Student') {
+          this.student.push(res[i])
+        }        
+      }
+      console.log(this.student);
+    });
+  }
 }
